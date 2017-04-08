d5481ea51c206a2cc4556d2a5ee2b6a8d54f2b9d
Max <tutv95@gmail.com>
2017-04-08 16:38:27 +0700

    Added user service

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+import {UserService} from "../../services/user.service";
 
 @Component({
     selector: 'edoo-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
+    styleUrls: ['./login.component.scss'],
+    providers: [UserService]
 })
 export class LoginComponent implements OnInit {
     private user: any = {
@@ -11,14 +13,21 @@ export class LoginComponent implements OnInit {
         password: ''
     };
 
-    constructor() {
+    constructor(private userSrv: UserService) {
     }
 
     ngOnInit() {
     }
 
     submit() {
+        console.log(this.user);
 
+        this.userSrv.login(this.user.email, this.user.password)
+            .subscribe(
+                data => {
+                    console.log(data);
+                }
+            );
     }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,70 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http, Headers, Response} from "@angular/http";
+import {AuthService} from "./auth.service";
+import {StorageService} from "./storage.service";
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class ApiService {
 
-  constructor() { }
+    private BASE_URL = '';
 
+    constructor(private http: Http,
+                private authSrv: AuthService,
+                private storageSrv: StorageService) {
+        this.BASE_URL = environment.api;
+    }
+
+    public requestAuth(args, silent: boolean = false) {
+        let token = this.storageSrv.getToken();
+        let access_token = token.access_token || false;
+        let headers = args.headers || {};
+        headers['Authorization'] = access_token;
+
+        args.headers = headers;
+
+        return this.request(args);
+    }
+
+    public request(args): Observable<any> {
+        let url_api = this.BASE_URL + args.url;
+
+        let headers = new Headers();
+        headers.set('Content-Type', 'application/json');
+        let args_headers = args.headers || {};
+        for (let header_key in args_headers) {
+            headers.set(header_key, args_headers[header_key]);
+        }
+
+        let request = this.http
+            .request(url_api, {
+                method: args.method,
+                headers: headers,
+                body: args.data
+            })
+            .map(
+                (response: Response) => {
+                    return response.json();
+                }
+            )
+            .share();
+
+        this.subscribeInvalidToken(request);
+
+        return request;
+    }
+
+    private subscribeInvalidToken(request: Observable<any>) {
+        request
+            .subscribe(
+                () => {
+                },
+                error => {
+                    if (error.status && error.status == 401) {
+                        this.authSrv.$invalidToken.next();
+                    }
+                }
+            );
+    }
 }
