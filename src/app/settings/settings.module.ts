f37697e372a834f9edaf94f837891ab8aa9ca523
Max <tutv95@gmail.com>
2017-04-16 12:58:48 +0700

    Updated sidebar settings

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -3,6 +3,8 @@ import {CommonModule} from '@angular/common';
 import {SettingsRoutingModule} from "./settings.routing";
 import {SidebarUserSettingsComponent} from './sidebar-user-settings/sidebar-user-settings.component';
 import {SettingsLayoutMasterComponent} from './settings-layout-master/settings-layout-master.component';
+import {EditProfileComponent} from './edit-profile/edit-profile.component';
+import {ChangePasswordComponent} from './change-password/change-password.component';
 
 @NgModule({
     imports: [
@@ -11,7 +13,9 @@ import {SettingsLayoutMasterComponent} from './settings-layout-master/settings-l
     ],
     declarations: [
         SettingsLayoutMasterComponent,
-        SidebarUserSettingsComponent
+        SidebarUserSettingsComponent,
+        EditProfileComponent,
+        ChangePasswordComponent
     ]
 })
 export class SettingsModule {
--- src/app/settings/settings.routing.ts
+++ src/app/settings/settings.routing.ts
@@ -1,13 +1,29 @@
 import {NgModule} from "@angular/core";
 import {Routes, RouterModule} from "@angular/router";
 import {SettingsLayoutMasterComponent} from "./settings-layout-master/settings-layout-master.component";
+import {EditProfileComponent} from "./edit-profile/edit-profile.component";
+import {ChangePasswordComponent} from "./change-password/change-password.component";
 
 
 export const routes: Routes = [
     {
         path: '',
         component: SettingsLayoutMasterComponent,
-        children: []
+        children: [
+            {
+                path: '',
+                redirectTo: 'profile',
+
+            },
+            {
+                path: 'profile',
+                component: EditProfileComponent
+            },
+            {
+                path: 'change-password',
+                component: ChangePasswordComponent
+            }
+        ]
     }
 ];
 
