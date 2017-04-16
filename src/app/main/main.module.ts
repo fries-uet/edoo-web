97ce5dc463b11385c02ff74fd63e82b52ab4893b
Max <tutv95@gmail.com>
2017-04-16 09:13:49 +0700

    added list classes

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,13 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MainRoutingModule} from "./main.routing";
+import { ListClassesComponent } from './list-classes/list-classes.component';
 
 @NgModule({
     imports: [
         CommonModule,
         MainRoutingModule
     ],
-    declarations: []
+    declarations: [ListClassesComponent]
 })
 export class MainModule {
 }
--- src/app/main/main.routing.ts
+++ src/app/main/main.routing.ts
@@ -1,8 +1,14 @@
 import {NgModule} from "@angular/core";
 import {Routes, RouterModule} from "@angular/router";
+import {ListClassesComponent} from "./list-classes/list-classes.component";
 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListClassesComponent
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
