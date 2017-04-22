0978e769b855286703944c80f793169e58fbf289
Max <tutv95@gmail.com>
2017-04-22 13:01:15 +0700

    Updated navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 import {AuthService} from "../../services/auth.service";
-import {EdClass} from "../../definitions/ed-class";
 
 @Component({
     selector: 'edoo-navbar',
