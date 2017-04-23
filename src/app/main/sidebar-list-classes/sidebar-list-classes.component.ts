c0998f821427146f92536863a25a562ce06906ba
Max <tutv95@gmail.com>
2017-04-23 09:55:48 +0700

    Updated sidebar list classes

--- src/app/main/sidebar-list-classes/sidebar-list-classes.component.ts
+++ src/app/main/sidebar-list-classes/sidebar-list-classes.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ClassService} from "../../services/class.service";
+import {EdClass} from "../../definitions/ed-class";
 
 @Component({
-  selector: 'ed-sidebar-list-classes',
-  templateUrl: './sidebar-list-classes.component.html',
-  styleUrls: ['./sidebar-list-classes.component.scss']
+    selector: 'ed-sidebar-list-classes',
+    templateUrl: './sidebar-list-classes.component.html',
+    styleUrls: ['./sidebar-list-classes.component.scss']
 })
 export class SidebarListClassesComponent implements OnInit {
+    public listClasses: Array<EdClass> = [];
 
-  constructor() { }
+    constructor(private classSrv: ClassService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        this.classSrv.getListClasses()
+            .subscribe(
+                response => {
+                    let data = response.data;
+                    this.listClasses = data.classes;
+                }
+            );
+    }
 
 }
